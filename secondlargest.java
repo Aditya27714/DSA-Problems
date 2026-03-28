4d1ab06d9f5e2bfa4717e3fbf1e98d1fc6f228cf
Aditya <aadityasingh9713@gmail.com>
2026-03-28 22:20:16 +0530

    Second largest no . in array

---
+++ secondlargest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class secondlargest {
+    
+     public static  void main(String[] args) {
+        System.out.println("Enter the size of array ");
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println("Enter the array ");
+        int [] arr =new int[n];
+        for(int i=0;i<n;i++){
+           arr[i]=sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        System.out.println("Second largest element is"+ arr[n-2]);
+
+}
+}
