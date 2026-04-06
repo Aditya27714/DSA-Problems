6a1f8f940ceaebad6e532f2c643b88d7f08ae421
Aditya <aadityasingh9713@gmail.com>
2026-04-06 22:55:54 +0530

    L 26

---
+++ Removeduplicate.java
@@ -0,0 +1,25 @@
+
+// leetcode Q.26
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int count = 0;
+        for(int i = 0;i<nums.length -1 ;i++){
+            if(nums[i]==nums[i+1]){
+                continue ;
+            }
+           
+                nums[count]=nums[i];
+                count ++ ;
+            
+        }
+            nums[count] = nums[nums.length - 1];
+            // count++ ;
+       
+
+        return count + 1 ;
+
+    }
+}
