31d3dbf217a8bb6486054aaa60a98c58e5576c9d
Aditya <aadityasingh9713@gmail.com>
2026-04-21 23:30:41 +0530

    L  206

---
+++ Reverselinkedlist.java
@@ -0,0 +1,21 @@
+//Leetcode Q.206
+
+
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+     
+   ListNode prev = null;
+    ListNode current = head;
+    ListNode next = null;
+
+    while (current != null){
+        next = current.next;   
+        current.next = prev;   
+        prev = current;       
+        current = next;      
+    }
+    return prev; 
+}
+
+    }
