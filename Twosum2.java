92b25456c3fa3b8fb76fb1efd83c99be00672bfc
Aditya <aadityasingh9713@gmail.com>
2026-04-24 21:30:10 +0530

    L  167

---
+++ Twosum2.java
@@ -0,0 +1,25 @@
+// Leetcode Q.167
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left =0;
+        int right=numbers.length-1;
+     
+      while(left<right){
+            if(numbers[left]+numbers[right]==target){
+                left++;
+                right++;
+             break;
+              
+            }
+            else if (numbers[left]+numbers[right]>target){
+                right-- ;
+            }
+            else if (numbers[left]+numbers[right]<target){
+                left++;
+            }
+        }
+
+        return new int []{left,right} ;
+    }
+}
