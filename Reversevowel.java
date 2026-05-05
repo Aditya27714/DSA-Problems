dd680be287ab9468f01a5ed77018223851797582
Aditya <aadityasingh9713@gmail.com>
2026-05-05 22:14:42 +0530

    L  345

---
+++ Reversevowel.java
@@ -0,0 +1,34 @@
+//Leetcode  Q.345 
+
+
+import java.util.*;
+class Solution {
+    public String reverseVowels(String s) { 
+      Set <Character> val = new HashSet <>(Arrays.asList(
+            'a','e','i','o','u','A','E','I','O','U'
+        ));
+          char[] chars = s.toCharArray();
+          int left = 0;
+          int right = chars.length -1 ;
+        while(left<=right){
+            if(!val.contains(chars[left])){
+                left ++ ;
+            }
+            else if(!val.contains(chars[right])){
+                right -- ;
+
+            }
+            else{
+                char temp= chars[left];
+                chars[left] = chars[ right];
+                chars[right]=temp;
+                left ++;
+                right --;
+            }
+        }
+
+        return new String(chars);
+
+        }
+
+    }
