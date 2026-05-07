c58d0e79c7bf02b42b72696e1cd8764fe3b699d5
Aditya <aadityasingh9713@gmail.com>
2026-05-07 23:49:01 +0530

    L   1071

---
+++ Greatest.java
@@ -0,0 +1,34 @@
+//Leetcode Q.1071
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+     
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+       
+        int leng = gcd(str1.length(), str2.length());
+        return str1.substring(0, leng);
+    }
+
+     private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+
+    
+}
+    
+}
+
+    
+
+    
+
+    
+
+
+    
