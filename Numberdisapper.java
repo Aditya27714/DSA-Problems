68fa2f790bdc642e2ac53dcbbaaaab48daf1ba6a
Aditya <aadityasingh9713@gmail.com>
2026-05-08 23:16:15 +0530

    L  448

---
+++ Numberdisapper.java
@@ -0,0 +1,20 @@
+//Leetcode Q.448
+
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+   Set <Integer> set = new HashSet<>();
+   List <Integer> list = new  ArrayList<>();
+
+   for(int num : nums){
+    set.add(num);
+   }
+   for(int i = 1; i<=nums.length;i++){
+    if(!set.contains(i)){
+        list.add(i);
+    }
+   }
+   return list ;
+
+    }
+}
