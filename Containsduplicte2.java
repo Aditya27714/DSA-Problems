d962ec3a5919d72444e12d20dc51ce74665577e3
Aditya <aadityasingh9713@gmail.com>
2026-06-10 23:32:44 +0530

    L  219

---
+++ Containsduplicte2.java
@@ -0,0 +1,32 @@
+//Leetcode Q.219
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+         Set<Integer> window = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (window.contains(nums[i])) {
+                return true;
+            }
+            window.add(nums[i]);
+            
+           
+            if (window.size() > k) {
+                window.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
+
+//         for (int i=0;i<nums.length;i++){
+//             for (int j=i+1;j<nums.length;j++){
+//                 if(nums[i]==nums[j] && Math.abs(i - j) <= k){
+//                     return true;
+//                 }
+//         }
+//         }
+//      return false;   
+//     }
+// }
