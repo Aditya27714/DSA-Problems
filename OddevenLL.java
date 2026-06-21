c0a34427ab555e25964a2ce44058ac09ab5b246b
Aditya <aadityasingh9713@gmail.com>
2026-06-21 22:56:46 +0530

    L  328

---
+++ OddevenLL.java
@@ -0,0 +1,24 @@
+//Leetcode Q.328
+
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        
+        if(head==null || head.next==null ) return head;
+
+        ListNode odd=head;
+        ListNode even=head.next;
+         ListNode even_head=even;
+
+        while(even!=null &&  even.next!=null){
+        odd.next=odd.next.next;
+          odd=odd.next;
+          even.next=even.next.next;
+          even=even.next;
+
+        }
+        odd.next=even_head;
+
+        return head;
+    }
+}
