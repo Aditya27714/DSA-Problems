de7e394e18623b52a42118db1b4548a9c70a821a
Aditya <aadityasingh9713@gmail.com>
2026-06-29 23:36:37 +0530

    L  1290

---
+++ Convertbinary.java
@@ -0,0 +1,16 @@
+//Leetcode Q.1290
+
+class Solution {
+    public int getDecimalValue(ListNode head) {
+     
+     int result=0;
+
+     while(head!=null)
+     {
+        result=result*2+head.val;
+        head=head.next;
+     }
+
+     return result;   
+    }
+}
