bf476eb3d3005f0fae28cfe4a9d9c5abc23ba635
Aditya <aadityasingh9713@gmail.com>
2026-07-03 23:10:37 +0530

    L  1464

---
+++ Maxiproduct.java
@@ -0,0 +1,19 @@
+//Leetcode Q.1464
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                int temp=((nums[i]-1)*(nums[j]-1));
+                 if(count<temp)
+                 {
+                    count=temp;
+                 }
+             }
+        }
+        return count;
+    }
+}
