4a0a71157e2215ad60f9d883d911ab21b922c8f4
Aditya <aadityasingh9713@gmail.com>
2026-07-07 23:28:46 +0530

    L   2095

---
+++ Deletemidll.java
@@ -0,0 +1,38 @@
+//Leetcode Q.2095
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+
+ if (head == null || head.next == null) return null;
+        int count =0;
+        ListNode curr=head;
+
+        while(curr!=null)
+        {
+            count++;
+            curr=curr.next;
+        }
+        int midnode=count/2;
+        curr=head;
+
+        for(int i=0;i<midnode-1;i++)
+        {
+            curr=curr.next;
+        }
+
+        curr.next=curr.next.next;
+        
+        return head;
+    }
+}
