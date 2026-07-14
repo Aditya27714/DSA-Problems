0a19d30520d514662fb11ea7965805a6e22bbccd
Aditya <aadityasingh9713@gmail.com>
2026-07-14 23:19:47 +0530

    L  817

---
+++ LLcomponent.java
@@ -0,0 +1,38 @@
+//Leetcode Q.  817
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+
+     
+        HashSet<Integer> set = new HashSet<>();
+        for(int n: nums)
+        {
+            set.add(n);
+        }
+          
+        int count = 0;
+        ListNode curr = head;
+        
+        while (curr != null) 
+        {
+            if (set.contains(curr.val) &&  (curr.next == null || !set.contains(curr.next.val))) {
+                count++;
+            }
+            curr = curr.next;
+        }
+        
+        return count;
+
+        
+    }
+}
