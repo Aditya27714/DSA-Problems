3d66861f9c92916d93bbc3399b9d0065d219fcce
Aditya <aadityasingh9713@gmail.com>
2026-07-27 22:43:58 +0530

    L  725

---
+++ Splitll.java
@@ -0,0 +1,55 @@
+//Leetcode Q. 725
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+               ListNode[] result = new ListNode[k];
+        
+    
+        int n = 0;
+        ListNode curr = head;
+        while (curr != null)
+        {
+            n++;
+            curr = curr.next;
+        }
+        
+     
+        int baseSize = n / k;
+        int extra = n % k;
+        
+      
+        curr = head;
+        for (int i = 0; i < k; i++) 
+        {
+            result[i] = curr;
+            int partSize = baseSize + (i < extra ? 1 : 0);
+            
+       
+            for (int j = 0; j < partSize - 1 && curr != null; j++) 
+            {
+                curr = curr.next;
+            }
+           
+            if (curr != null)
+            {
+                ListNode next = curr.next;
+                curr.next = null;
+                curr = next;
+            }
+        }
+        
+        return result;
+    }
+}
+
+        
