e82bc3475419d846e00ded395ebce9a06368dcfd
Aditya <aadityasingh9713@gmail.com>
2026-07-31 23:30:40 +0530

    L  16

---
+++ 3sumcloset.java
@@ -0,0 +1,36 @@
+// leetcode Q. 16
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+       
+       Arrays.sort(nums);
+       int n=nums.length;
+       int best = nums[0] + nums[1] + nums[2];
+   
+    for (int i = 0; i < n - 2; i++) 
+    {
+         int left=i+1;
+       int right=n-1;
+
+      while(left<right)
+      {
+        int sum=nums[i]+nums[left]+nums[right];
+        if (Math.abs(sum - target) < Math.abs(best - target)) {
+                  best = sum;
+      }
+     if(sum==target) return sum;
+
+     else if (sum>target)
+     {
+        right--;
+     }
+     else
+     {
+        left++;
+     }
+
+      }
+   }
+   return best;
+    }
+}
